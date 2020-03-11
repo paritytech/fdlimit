005bcacbbfe74d1e90593c3cc18e5064afa4d6c9
NikVolf <nikvolf@gmail.com>
2020-03-11 14:48:22 +0300

    bump to 0.1.4

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 // Copyright 2016-2020 Parity Technologies (UK) Ltd.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,7 +21,6 @@ extern crate libc;
 #[cfg(any(target_os = "macos", target_os = "ios"))]
 #[allow(non_camel_case_types)]
 pub fn raise_fd_limit() {
-	use libc;
 	use std::cmp;
 	use std::io;
 	use std::mem::size_of_val;
